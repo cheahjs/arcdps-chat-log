06c5814801d05f4b80067f9400f3979bff786296
Jun Siang Cheah <me@jscheah.me>
2022-12-30 20:11:09 +0000

    fix tag script not including Cargo.lock

--- .github/tag-and-push.sh
+++ .github/tag-and-push.sh
@@ -7,10 +7,11 @@ VERSION=$1
 echo "Bumping version to ${VERSION}"
 
 cargo set-version "$VERSION"
+cargo generate-lockfile --offline
 
 echo "Committing changes"
 
-git add Cargo.toml
+git add Cargo.toml Cargo.lock
 git commit -m "Updated version to ${VERSION}"
 git tag -a "$VERSION" -m "$VERSION"
 git push --tags
