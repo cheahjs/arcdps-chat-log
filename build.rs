724be8f38465da27fa56b5ab32c7a9e0fab0e02a
Jun Siang Cheah <git@jscheah.me>
2025-12-23 12:25:04 +0000

    chore: enable the ability to cross compile from mac (#31)

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
-    let res = winres::WindowsResource::new();
-    res.compile().unwrap();
+    if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap_or_default() == "windows" {
+        let mut res = winres::WindowsResource::new();
+        if let Ok(windres) = std::env::var("WINDRES") {
+            res.set_windres_path(&windres);
+        }
+        res.compile().unwrap();
+    }
 }
